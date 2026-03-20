c2cf53fdf8da90e9918db6e6f7ed57497c54c276
Vukasin Markovic <skolskiacc@gmail.com>
2026-03-20 19:26:15 +0100

    feat(api): Gin HTTP server(added prices handler) + move CoinGeckoLoop to background (gorutine)
    - Gin API (:8080) + goroutine CoinGeckoLoop() (1min intervals)
    - /api/v1/prices/:symbol -> onchain + chainlink prices (from contract)
    - /api/v1/health -> status

---
+++ app/api/handler.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"net/http"
+
+	"Blockchain-PriceOracle/app/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PriceResponse struct {
+	Symbol       string `json:"symbol"`
+	OnChainPrice string `json:"onchain_price"`
+	Chainlink    string `json:"chainlink_price"` // I added this
+	// TO DO - add coingecko response
+}
+
+func GetPricesHandler(c *gin.Context) {
+	symbol := c.Param("symbol")
+
+	client := utils.GetOracleClient()
+
+	onChainPrice, err := client.GetOnChainPrice(symbol)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch onchain price: " + err.Error(),
+		})
+		return
+	}
+
+	chainlinkPrice, err := client.GetChainlinkPrice(symbol)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch Chainlink price: " + err.Error(),
+		})
+		return
+	}
+
+	response := PriceResponse{
+		Symbol:       symbol,
+		OnChainPrice: onChainPrice.String(),
+		Chainlink:    chainlinkPrice.String(),
+	}
+
+	c.JSON(http.StatusOK, response)
+}
--- app/main.go
+++ app/main.go
@@ -6,15 +6,48 @@ import (
 	"math/big"
 	"time"
 
+	"Blockchain-PriceOracle/app/api"
 	"Blockchain-PriceOracle/app/utils"
 	"Blockchain-PriceOracle/internal/coingecko"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func printPrices(prices *map[string]*big.Int) {
 	for symbol, price := range *prices {
 		fmt.Printf("price -> %s: $%s\n", symbol, price.String())
+	}
+}
+
+func CoinGeckoLoop() {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	log.Println("CoinGecko loop: every 1min (background)")
+	fmt.Println("----------------------------")
+	fmt.Println("1min intervals started - CoinGecko")
+
+	for {
+		fmt.Println("\n=== NEW MINUTE ===")
+
+		cgPrices, err := coingecko.FetchPrices()
+		if err != nil {
+			log.Printf("Fetch error: %v\n", err)
+			// TO DO fallback prices
+			<-ticker.C
+			continue
+		}
+
+		printPrices(&cgPrices)
+
+		for symbol, price := range cgPrices {
+			if utils.CheckPriceCriteria(symbol, price) {
+				log.Printf("%s - SEND TX NOW!\n", symbol)
+			}
+		}
+
+		<-ticker.C
 	}
 }
 
@@ -28,30 +61,20 @@ func main() {
 	// utils.TestOracle()
 
 	// -------------------------------------------
-	fmt.Println("----------------------------")
-	fmt.Println("CoinGecko Test - PRICE FETCH")
 
-	ticker := time.NewTicker(1 * time.Minute)
-	for {
-		fmt.Println("\n=== NEW MINUTE ===")
+	go CoinGeckoLoop()
 
-		cgPrices, err := coingecko.FetchPrices()
-		if err != nil {
-			fmt.Printf("Fetch error: %v\n", err)
-			// TO DO fallback prices
-			<-ticker.C
-			continue
-		}
+	r := gin.Default()
 
-		printPrices(&cgPrices)
-
-		for symbol, price := range cgPrices {
-			if utils.CheckPriceCriteria(symbol, price) {
-				fmt.Printf("%s - SEND TX NOW!\n", symbol)
-			}
-		}
-
-		<-ticker.C
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/prices/:symbol", api.GetPricesHandler)
+		v1.GET("/health", func(c *gin.Context) {
+			c.JSON(200, gin.H{"status": "OK", "price_checker": "running"})
+		})
 	}
 
+	log.Println("API Server: http://localhost:8080")
+
+	log.Fatal(r.Run(":8080"))
 }
